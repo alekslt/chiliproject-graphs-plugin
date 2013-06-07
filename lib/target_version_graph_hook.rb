c399962957b80e4772c41fb536729689c6cd7148
Lynx Administrator <lynxadm@lynx.hrp.no>
2013-06-07 11:12:36 +0200

    Really added the changed views this time

--- lib/target_version_graph_hook.rb
+++ lib/target_version_graph_hook.rb
@@ -7,18 +7,19 @@ class TargetVersionGraphHook < Redmine::Hook::ViewListener
       #output << "</fieldset>"
 
       output = "<fieldset id='target_version_graph'>\n"
-      output << "<a href=" << url_for(:controller => 'graphs', :action => 'target_version_status_graphjs', :id => context[:version]) << "?size=large" << ">\n"
+      output << "<a href=\"" << url_for(:controller => 'graphs', :action => 'target_version_status_graphjs', :id => context[:version]) << "?size=large" << "\">\n"
       output << "<legend>#{ l(:label_graphs_status) }</legend>\n"
-      output << tag("embed", :width => "100%", :height => 500, :type => "text/html", :src => url_for(:controller => 'graphs', :action => 'target_version_status_graphjs', :id => context[:version]) << "?size=small" )
+      output << content_tag("object", "<p>Your browser does not support the object tag</p>",  :width => "100%", :height => 500, :type => "text/html", :data => url_for(:controller => 'graphs', :action => 'target_version_status_graphjs', :id => context[:version]) << "?size=small" )
       output << "</a>\n"
       output << "</fieldset>\n"
 
       output << "<fieldset id='target_version_graph_last15'>\n"
-      output << "<a href=" << url_for(:controller => 'graphs', :action => 'target_version_status_graphjs', :id => context[:version]) << "?size=large&lastdays=15" << ">\n"
+      output << "<a href=\"" << url_for(:controller => 'graphs', :action => 'target_version_status_graphjs', :id => context[:version]) << "?size=large&lastdays=15" << "\">\n"
       output << "<legend>#{ l(:label_graphs_status_last) }</legend>\n"
-      output << tag("embed", :width => "100%", :height => 500, :type => "text/html", :src => url_for(:controller => 'graphs', :action => 'target_version_status_graphjs', :id => context[:version]) << "?size=small&lastdays=15" )
+      output << content_tag("object", "<p>Your browser does not support the object tag</p>", :width => "100%", :height => 500, :type => "text/html", :data => url_for(:controller => 'graphs', :action => 'target_version_status_graphjs', :id => context[:version]) << "?size=small&lastdays=15" )
       output << "</a>\n"
       output << "</fieldset>\n"
+
 
 		return output
 	end 
